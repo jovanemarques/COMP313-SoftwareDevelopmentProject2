a7942d185f6ef02544a610957f6484e54d40a90e
Jovane Marques <jovanemarques@gmail.com>
2020-04-15 18:34:37 -0400

    cascading item delete to remove it from recipe

--- models/item.model.js
+++ models/item.model.js
@@ -7,6 +7,23 @@ var ItemSchema = new Schema({
     baseUnit: String,
 });
 
+ItemSchema.pre('remove', { document: true, query: false }, function() {
+    const that = this;
+    const Recipe = require('./recipe.model');
+    Recipe.find({items: that._id.toJSON()}, function (err, docs) {
+        if (err) return console.log(err);
+        if (docs && docs.length > 0){
+            docs.forEach(d => {
+                d.items.pull(that._id);
+                d.save(function (err2, recipe) {
+                    if (err2) return console.log(err2);
+                });
+            });
+        }
+    });
+    //delete report
+});
+
 ItemSchema.set('toJSON', {
 	getters: true,
 	virtuals: true
