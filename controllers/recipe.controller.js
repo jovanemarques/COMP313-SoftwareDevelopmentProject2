aad66538f656858f9192f6b782c14df082a3c984
Jovane Marques <jovanemarques@gmail.com>
2020-04-15 15:49:23 -0400

    new API to list recipes with items

--- controllers/recipe.controller.js
+++ controllers/recipe.controller.js
@@ -7,4 +7,25 @@ function RecipeController() {
 
 RecipeController.prototype = Object.create(BaseController.prototype);
 
+RecipeController.prototype.listWithItems = function(req, res) {
+    const page = parseInt(req.query.page) || 1;
+    const size = parseInt(req.query.size) || 0;
+    if (req.params.id) {
+        Recipe
+        .findById(req.params.id)
+        .populate('items')
+        .exec(function(err, model) {
+            if (err) return res.status(500).send(err);
+            res.send(model);
+        }); 
+    } else {
+        Recipe.find({}, function(err, model) {
+            if (err) return res.status(500).send(err);
+            res.json(model);
+        })
+        .skip(size * (page - 1)) 
+        .limit(size)
+    }
+  };
+
 module.exports = RecipeController;
--- routes/recipe.routes.js
+++ routes/recipe.routes.js
@@ -6,6 +6,7 @@ const user_controller = new UserController();
 const RecipeController = require('../controllers/recipe.controller');
 const recipe_controller = new RecipeController();
 
+router.get('/recipe/listWithItems/:id', user_controller.requiresLogin, recipe_controller.listWithItems.bind(recipe_controller));
 router.get('/recipe/:id?', user_controller.requiresLogin, recipe_controller.list.bind(recipe_controller));
 router.post('/recipe', user_controller.requiresLogin, recipe_controller.add.bind(recipe_controller));
 router.put('/recipe/:id', user_controller.requiresLogin, recipe_controller.update.bind(recipe_controller));
