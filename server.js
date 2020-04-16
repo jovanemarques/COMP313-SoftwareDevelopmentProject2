49baa3bf0c343c7dd64de4506de11036b2481539
Jovane Marques <jovanemarques@gmail.com>
2020-04-15 21:23:35 -0400

    AWS Changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var mongoose = require('./config/mongoose');
 var express = require('./config/express');
 var db = mongoose();
 var app = express();
-const port = process.env.port || 3001
+const port = process.env.PORT || 3001
 app.listen(port);
 module.exports = app;
-console.log(`Server running at http://localhost:${port}/`);
+console.log(`Server running at http://localhost:${port}`);
